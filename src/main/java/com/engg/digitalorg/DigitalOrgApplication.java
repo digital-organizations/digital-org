818301c4b7caf8fb06a9c01bc5916f68c6921302
Abhay Shukla <abhay.shukla04@gmail.com>
2020-09-05 10:45:26 +0530

    Initial commit

---
+++ src/main/java/com/engg/digitalorg/DigitalOrgApplication.java
@@ -0,0 +1,13 @@
+package com.engg.digitalorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalOrgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalOrgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/engg/digitalorg/DigitalOrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engg.digitalorg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalOrgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
