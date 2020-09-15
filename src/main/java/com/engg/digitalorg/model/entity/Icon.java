2869bf0be5d7377d40ed0e53819216ca14f7662d
Abhay Shukla <abhay.shukla04@gmail.com>
2020-09-15 19:30:31 +0530

    fix codacy warning

--- src/main/java/com/engg/digitalorg/model/entity/Icon.java
+++ src/main/java/com/engg/digitalorg/model/entity/Icon.java
@@ -3,8 +3,12 @@ package com.engg.digitalorg.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-
+import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Lob;
 @Getter
 @Setter
 @Entity
--- src/main/java/com/engg/digitalorg/model/entity/UserInGroup.java
+++ src/main/java/com/engg/digitalorg/model/entity/UserInGroup.java
@@ -3,7 +3,11 @@ package com.engg.digitalorg.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import java.util.Date;
 
 @Getter
